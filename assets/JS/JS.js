b58bf9316c61fa433ea3837b906036391a152a28
alexianexad <alexianarphexad@icloud.com>
2025-03-09 17:15:26 +0100

    Add files via upload

---
+++ assets/JS/JS.js
@@ -0,0 +1,12 @@
+function demanderNom() {
+    let nom = prompt("Quel est votre nom ?");
+    if (nom) {
+        alert("Bonjour, " + nom + " !");
+    } else {
+        alert("Vous n'avez pas entré de nom.");
+    }
+}
+
+function afficherAide() {
+    alert("Bienvenue sur mon site !");
+}
