e31652ed2cd0e9b8ad829a4c36b3c328c7d58ac6
alexianexad <alexianarphexad@icloud.com>
2025-03-24 23:18:38 +0100

    Update JS.js

--- assets/JS/JS.js
+++ assets/JS/JS.js
@@ -1,12 +1,7 @@
-function demanderNom() {
-    let nom = prompt("Quel est votre nom ?");
-    if (nom) {
-        alert("Bonjour, " + nom + " !");
-    } else {
-        alert("Vous n'avez pas entré de nom.");
-    }
+fonction prenom () {
+let name = document.getElementById("Comment vous appelez vous?").value;
+    document.getElementById("holder1").innerHTML = name;
 }
-
 function afficherAide() {
     alert("Bienvenue sur mon site !");
 }
