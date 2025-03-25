8a2981fc6290263d762644317bd9f895618470d4
alexianexad <alexianarphexad@icloud.com>
2025-03-25 01:00:58 +0100

    Update JS.js

--- assets/JS/JS.js
+++ assets/JS/JS.js
@@ -1,3 +1,12 @@
+fonction demanderNom() {
+    let nom = prompt("Quel est vetre nom?");
+    if (nom) {
+        alert("Bonjour,"+ nom+"!");
+    } else {
+        alert("Vous n'avez pas entré de nom.);
+              }
+}
+
 fonction prenom () {
 let name = document.getElementById("Comment vous appelez vous?").value;
     document.getElementById("holder1").innerHTML = name;
